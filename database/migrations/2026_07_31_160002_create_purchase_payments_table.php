c2970ca19ba08222a65c9accba1d7c99af7c50b5
Ly Kimphev <lykimphev@gmail.com>
2026-07-31 19:27:38 +0700

    Create 2026_07_31_160002_create_purchase_payments_table.php

---
+++ database/migrations/2026_07_31_160002_create_purchase_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('MethodID')->constrained('payment_methods', 'id')->onDelete('cascade');
+            $table->foreignId('PurchaseID')->constrained('purchases', 'id')->onDelete('cascade');
+            $table->decimal('TotalPayment', 18, 6);
+            $table->date('PurchaseDate');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_payments');
+    }
+};
