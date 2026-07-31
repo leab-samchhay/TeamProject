01b571c8d3c0d8a05a8e7c26c0e3ecf8fb32fdc9
Ly Kimphev <lykimphev@gmail.com>
2026-07-31 15:27:59 +0700

    Update 2026_07_23_140749_create_customers_table.php

--- database/migrations/2026_07_23_140749_create_customers_table.php
+++ database/migrations/2026_07_23_140749_create_customers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->default('general');
             $table->string('sex')->nullable();
             $table->string('phone')->nullable();
             $table->boolean('status')->default(true);
