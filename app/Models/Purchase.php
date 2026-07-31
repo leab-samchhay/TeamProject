f82af4462a640be67f2abfa04d8c817596cb8bd1
Ly Kimphev <lykimphev@gmail.com>
2026-07-31 15:27:35 +0700

    Create Purchase.php

---
+++ app/Models/Purchase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Purchase extends Model
+{
+    use SoftDeletes;
+    protected $table = 'puchases';
+
+    protected $fillable = [
+        'buillno', 'puchaseDate', 'discound', 'totalAmount', 'status',
+        'supplierId', 'userId',
+    ];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'supplierId');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+}
