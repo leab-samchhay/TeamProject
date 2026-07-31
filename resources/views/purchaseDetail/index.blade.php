05f93f067c7ee15fd72ca09c716ed36afd49a504
Ly Kimphev <lykimphev@gmail.com>
2026-07-31 15:28:32 +0700

    Create index.blade.php

---
+++ resources/views/purchaseDetail/index.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.app')
+@section('title', 'PurchaseDetail')
+@section('content')
+    <div class="row my-2">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header d-flex align-items-center justify-content-between border-bottom border-light">
+                    <h4 class="header-title">Manage PurchaseDetail</h4>
+                    <div>
+                        <a href="{{ route('purchaseDetail.create') }}" class="btn btn-success bg-gradient"><i
+                            class="ti ti-plus me-1"></i> Add PurchaseDetail</a>
+
+                    </div>
+                </div>
+
+                <div class="table-responsive">
+                    <table class="table table-nowrap mb-0">
+                        <thead class="bg-light-subtle">
+                            <tr>
+                                <th class="ps-3" style="width: 60px;">
+                                    No
+                                </th>
+                                <th>PruchaseID</th>
+                                <th>ProductID</th>
+                                <th>Quantity</th>
+                                <th>Cost</th>
+                                <th>Discount</th>
+                                <th class="text-center" style="width: 125px;">Action</th>
+                            </tr>
+                        </thead><!-- end thead -->
+                        <tbody>
+                            @if ($rows)
+                                @foreach ($rows as $row )
+                                    <tr>
+                                        <td class="text-center">{{ $loop->iteration }}</td>
+                                        <td>{{ $row->buillno ?? '-' }}</td>
+                                        <td>{{ $row->productID ?? '-' }}</td>
+                                        <td>{{ $row->qty ?? '-' }}</td>
+                                        <td>{{ $row->cost ?? '-' }}</td>
+                                        <td>{{ $row->discound ?? '-' }}</td>
+                                        <td>
+                                            @if($row->status == true)
+                                                <button class="btn btn-sm text-white bg-success">
+                                                    Active
+                                                </button>
+                                            @else
+                                                <button class="btn btn-sm text-white bg-danger">
+                                                    Inactive
+                                                </button>
+                                            @endif
+                                        </td>
+
+                                        <td class="pe-3">
+                                            <div class="hstack gap-1 justify-content-end">
+
+                                                <a href="{{ route('purchaseDetail.edit',$row->id) }}"
+                                                    class="btn btn-soft-success btn-icon btn-sm rounded-circle"> <i
+                                                        class="ti ti-edit fs-16"></i></a>
+
+                                                <form action="{{ route('purchaseDetail.remove', $row->id) }}" method="POST"
+                                                    onsubmit="return confirm('Are you sure you want to delete this purchase?')"
+                                                    style="display:inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+
+                                                    <button type="submit"
+                                                        class="btn btn-soft-danger btn-icon btn-sm rounded-circle mt-0">
+                                                        <i class="ti ti-trash"></i>
+                                                    </button>
+                                                </form>
+
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                            @endif
+
+
+                        </tbody><!-- end tbody -->
+                    </table><!-- end table -->
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-end gap-5 align-items-center justify-conten-center">
+                        {{ $rows->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
